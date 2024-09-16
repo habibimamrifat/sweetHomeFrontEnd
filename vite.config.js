5fd01b8c30fd8695109e32035f01cc8c08bdf809
habibimamrifat <habibrifatx21@gmail.com>
2024-09-16 17:48:35 +0600

    port improved

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Railway's dynamic port if available
+  },
+  preview: {
+    port: process.env.PORT || 4173, // Use Railway's dynamic port for preview mode
+  }
 })
