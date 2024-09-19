3aadb4f96d95e686321c253ecb33cc151ff46fe5
habibimamrifat <habibrifatx21@gmail.com>
2024-09-19 12:22:26 +0600

    change password minor change

--- src/Pages/BakerPasswordReset.jsx
+++ src/Pages/BakerPasswordReset.jsx
@@ -5,7 +5,7 @@ const BakerPasswordReset = () => {
     
   return (
     <div>
-       <h2 className=' text-center text-5xl text-white mt-10'>Baker psassword Change</h2>
+       <h2 className=' text-center text-2xl lg:text-5xl text-white mt-10'>Baker psassword Change</h2>
 
       <PasswordReset
       position={"baker"}/>
--- src/Pages/CustomerPasswordReset.jsx
+++ src/Pages/CustomerPasswordReset.jsx
@@ -4,7 +4,7 @@ import PasswordReset from '../SharedComponents/PasswordReset'
 const CustomerPasswordReset = () => {
   return (
     <div>
-     <h2 className=' text-center text-5xl text-white mt-10'>Customer psassword Change</h2>
+     <h2 className=' text-center text-2xl lg:text-5xl text-white mt-10'>Customer psassword Change</h2>
       <PasswordReset
       position={"customer"}/>
     </div>
