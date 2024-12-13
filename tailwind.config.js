193fb6bb4158d984770f2cd7eea11e79afb7df36
habibimamrifat <habibrifatx21@gmail.com>
2024-12-14 00:04:22 +0600

    daisy ui taken care of

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
+import daisyui from 'daisyui'; // Import the DaisyUI plugin
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./components/**/*.{html,js}",
     "./pages/**/*.{html,js}",
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        "primary":'#0ea5e9',
+      colors: {
+        "primary": '#0ea5e9',
         "secondary": '#7dd3fc', // sky-300
         "shadowColor": '#075985', // shadow-sky-800
-        "successPrimary":"#4CBB17",
-        "successSecondary":"#90EE90"
-      }
+        "successPrimary": "#4CBB17",
+        "successSecondary": "#90EE90",
+      },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [daisyui], // Adding the DaisyUI plugin
 };
